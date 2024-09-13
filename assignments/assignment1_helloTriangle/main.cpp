ac75485669a6f75ee5a375a24a07d43d2c33aeca
StevenSpyro <steven.bruns@mymail.champlain.edu>
2024-09-13 11:08:34 -0400

    In class work
    Location thing is broken.

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -6,32 +6,38 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
-const int SCREEN_WIDTH = 1080;
-const int SCREEN_HEIGHT = 720;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
-float vertices[] = 
-{
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+float vertices[] =
+{   //X    //Y    //Z   //R   //G   //B   //A
+	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 
+	 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
 };
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec4 aColor;\n"
+"out vec4 Color;\n" //Varying
+"uniform float uTime;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   Color = aColor;\n" //Pass-Through
+"   vec3 pos = aPos;\n"
+"   pos.y += sin(uTime + pos.x);\n"
+"   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
 "}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec4 Color;\n"
+"uniform float uTime;\n"
+"uniform vec4 uColor = vec4(1.0);\n"
+"void main()\n"
+"{\n"
+"   FragColor = Color * (sin(uTime) * 0.5 + 0.5);\n"//vec4(1.0f, 0.5f, 0.2f, 1.0f);
+"}\n\0";
 
-const char* fragmentShaderSource = R"(
-#version 330 core
-out vec4 FragColor;
-
-void main()
-{
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-} 
-)";
 
 int main() {
 	printf("Initializing...");
@@ -49,22 +55,42 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
-	//Initialization goes here!
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
 
 	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//Initialization goes here!
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// 0. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 1. then set the vertex attributes pointers
+
+	//Position (XYZ)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//COLOR(RGBA)
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*) (sizeof(float) * 3));
+	glEnableVertexAttribArray(1);
+
+	//Vertex Shader
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
+
+
+	//Check for error
 	int  success;
 	char infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
 	if (!success)
 	{
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
@@ -72,19 +98,24 @@ int main() {
 	}
 	
 	//Create and complie fragment shader
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 
 	if (!success)
 	{
 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
 		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s", infoLog);
 	}
+
 	//Create shader program, link
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	unsigned int shaderProgram = glCreateProgram();
+
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
@@ -100,17 +131,56 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//someOpenGLFunctionThatDrawsOurTriangle();
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+	// 1. bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. then set our vertex attributes pointers
+
+
 	//Render loop
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
 		glfwPollEvents();
 		//Input
 		//Update
+		float time = (float)glfwGetTime();
+
 		//Draw
 		//Clear framebuffer
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+
+		//Set Time Uniform
+		int timeLoc = glGetUniformLocation(shaderProgram, "uTime");
+		glUniform1f(timeLoc, time);
+
+		glBindVertexArray(VAO); 
+
+		//Draw Call
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		//Drawing happens here!
+
 		glfwSwapBuffers(window);
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	printf("Shutting down...");
 }
---
+++ core/MyLibrary/shader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include"../ew/external/glad.h"
+
+namespace shader
+{
+	class Shader
+	{
+
+	};
+}
