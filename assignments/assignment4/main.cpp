9e0c101ca5614ac505ede9d60330d36c187d5c0e
Steven Bruns <steven.bruns@mymail.champlain.edu>
2024-10-19 20:17:19 -0400

    I've never been so happy to see a cube in my life.

--- assignments/assignment4/main.cpp
+++ assignments/assignment4/main.cpp
@@ -102,6 +102,37 @@ int main() {
 		1, 2, 3    // second triangle
 	};
 
+	glm::vec3 posRand[20];
+	for (int i = 0; i < 20; i++)
+	{
+		posRand[i].x = ew::RandomRange(-5.0, 5.0f);
+		posRand[i].y = ew::RandomRange(-5.0, 5.0f);
+		posRand[i].z = ew::RandomRange(-10.0, -5.0f);
+	}
+
+	float rotateAngleRand[20];
+	for (int i = 0; i < 20; i++)
+	{
+		rotateAngleRand[i] = ew::RandomRange(-179, 179);
+	}
+
+	glm::vec3 rotateAxisRand[20];
+	for (int i = 0; i < 20; i++)
+	{
+		rotateAxisRand[i].x = ew::RandomRange(-10.0, 10.0f);
+		rotateAxisRand[i].y = ew::RandomRange(-10.0, 10.0f);
+		rotateAxisRand[i].z = ew::RandomRange(-10.0, 0.0f);
+	}
+
+	glm::vec3 scaleRand[20];
+	for(int i = 0; i < 20; i++)
+	{
+		scaleRand[i].x = ew::RandomRange(1.0f, 20.0f);
+		scaleRand[i].y = ew::RandomRange(1.0f, 20.0f);
+		scaleRand[i].z = ew::RandomRange(1.0f, 20.0f);
+	}
+
+	/*
 	glm::vec3 cubePositions[] =
 	{
 			glm::vec3(0.0f,  0.0f,  0.0f),
@@ -115,6 +146,7 @@ int main() {
 			glm::vec3(1.5f,  0.2f, -1.5f),
 			glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
+	*/
 
 	//Initialization goes here!
 	unsigned int VBO, VAO, EBO;
@@ -145,7 +177,7 @@ int main() {
 
 	Texture2D texture0("assets/container.jpg", GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR, GL_REPEAT, GL_REPEAT, GL_RGB);
 	Texture2D texture1("assets/awesomeface.png", GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR, GL_REPEAT, GL_REPEAT, GL_RGB);
-	Texture2D texture2("assets/awesomeface.png", GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR, GL_REPEAT, GL_REPEAT, GL_RGB);
+	//Texture2D texture2("assets/awesomeface.png", GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR, GL_REPEAT, GL_REPEAT, GL_RGB);
 
 	bgShader.Shader::use();
 	bgShader.setInt("texture1", 0);
@@ -181,16 +213,16 @@ int main() {
 
 		//texture0.Bind(GL_TEXTURE0);
 
-		glm::mat4 view = glm::mat4(1.0f);
-		float radius = 10.0f;
-		float camX = static_cast<float>(sin(glfwGetTime()) * radius);
-		float camZ = static_cast<float>(cos(glfwGetTime()) * radius);
-		view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		//glm::mat4 view = glm::mat4(1.0f);
+		//float radius = 10.0f;
+		//float camX = static_cast<float>(sin(glfwGetTime()) * radius);
+		//float camZ = static_cast<float>(cos(glfwGetTime()) * radius);
+		//view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 
 
-		glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
+		//glm::mat4 model = glm::mat4(1.0f);
+		//glm::mat4 projection = glm::mat4(1.0f);
 
 		//projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 		//model = glm::rotate(model, time * glm::radians(45.0f), glm::vec3(0.5f, 1.0f, 0.0f));
@@ -200,10 +232,10 @@ int main() {
 		//int projectionLoc = glGetUniformLocation(bgShader.ID, "projection");
 		//bgShader.setMat4("projection", projection);
 
-		//glm::mat4 projection = glm::perspective(glm::radians(cam.Zoom), 800.0f / 600.0f, 0.1f, 1000.0f);
+		glm::mat4 projection = glm::perspective(glm::radians(cam.Zoom), 800.0f / 600.0f, 0.1f, 1000.0f);
 		bgShader.setMat4("projection", projection);
 
-		//glm::mat4 view = cam.GetViewMatrix();
+		glm::mat4 view = cam.GetViewMatrix();
 		bgShader.setMat4("view", view);
 
 
@@ -214,9 +246,10 @@ int main() {
 		{
 			// calculate the model matrix for each object and pass it to shader before drawing
 			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
+			model = glm::scale(model, scaleRand[i]);
+			model = glm::translate(model, posRand[i]);
 			float angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			model = glm::rotate(model, glm::radians(rotateAngleRand[i]), rotateAxisRand[i]);
 			bgShader.setMat4("model", model);
 
 			glDrawArrays(GL_TRIANGLES, 0, 36);
